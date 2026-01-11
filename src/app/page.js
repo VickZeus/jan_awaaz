3c93d211786c8e580d9a619cb6fe4285e9f04d2a
abhishek1604.official@gmail.com <abhishek1604.official@gmail.com>
2026-01-11 18:12:59 +0530

    homePageLogoAdded

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,28 @@ import {Atma,Zain} from "next/font/google"
 
 const atma=Atma({subsets:["latin"],weight:["400","700"]})
 const zain=Zain({subsets:["latin"],weight:["400","700"]})
+
+function Logo(){
+    return(
+        <div className={style.logo}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
 export default function HomePage(){
     return(
-        <div className={`${style.row_flex} ${style.layout} ${zain.className}`}>
+        <div className={`${style.row_flex} ${style.mblayout} ${zain.className}`}>
             <div className={`${style.title} ${atma.className}`}>JanAwaaz</div>
+            <Logo/>
             <div className={style.row_flex}>
                 <button className={style.options}>Contact</button>
                 <button className={style.options}>Help</button>
